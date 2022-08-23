e8237e99c634b1c26e034f51b4cae17378494d61
supri0343 <63761987+supri0343@users.noreply.github.com>
2022-08-23 14:26:47 +0700

    Revamp-QueryforTraceableIN

--- Com.DanLiris.Service.Purchasing.Lib/ViewModels/GarmentReports/TraceableInBeacukaiViewModel.cs
+++ Com.DanLiris.Service.Purchasing.Lib/ViewModels/GarmentReports/TraceableInBeacukaiViewModel.cs
@@ -37,7 +37,8 @@ namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.GarmentReports
         public double Subcon { get; set; }
         public string SampleOut { get; set; }
         public string UnitDOType { get; set; }
-
+        public string ExpenditureType { get; set; }
+        public long UENItemId { get; set; }
         public double WIP { get; set; }
 
 
@@ -120,44 +121,55 @@ namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.GarmentReports
 
     public class TraceableInWithBUMBeacukaiViewModel
     {
-        //public string BCNo { get; set; }
-        //public string BonNo { get; set; }
-        //public string BCType { get; set; }
-        //public DateTimeOffset BCDate { get; set; }
-        //public string ItemCode { get; set; }
-        //public string ItemName { get; set; }
-        //public string ProduksiQty { get; set; }
         public string URNNo { get; set; }
         public decimal SmallQty { get; set; }
-        //public double U { get; set; }
         public string SatuanReceipt { get; set; }
         public string UnitDONo { get; set; }
-
-
         public string ROJob { get; set; }
         public string UnitDOType { get; set; }
 
         public double DOQuantity { get; set; }
-        //public double EksporQty { get; set; }
         public string DOUomUnit { get; set; }
-        //public string SatuanWIP { get; set; }
-
         public string PO { get; set; }
-        //public string Invoice { get; set; }
-        //public DateTimeOffset? PEBDate { get; set; }
-        //public string PEB { get; set; }
+
         public string BUK { get; set; }
         public string UENType { get; set; }
         public double QtyBUK { get; set; }
         public string SatuanBUK { get; set; }
-
         public DateTime LastModifiedUtc { get; set; }
-        //public double SampleQty { get; set; }
-        //public double SubkonOutQty { get; set; }
-        //public string Sisa { get; set; }
-        //public string ExType { get; set; }
-        //public string Subcon { get; set; }
-        //public string SampleOut { get; set; }
+
+    }
+
+    public class TraceableInForToReceipt
+    {
+        public string BCNo { get; set; }
+        public string BCType { get; set; }
+        public DateTimeOffset BCDate { get; set; }
+        public string BonNo { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public double ReceiptQty { get; set; }
+        public string SatuanReceipt { get; set; }
+        public long URNItemId { get; set; }
+        public string POSerialNumber { get; set; }
+    }
+
+    public class TraceableInForToExpend
+    {
+        public string ROJob { get; set; }
+        public string ROSample { get; set; }
+        public string BUK { get; set; }
+        public double QtyBUK { get; set; }
+        public string SatuanBUK { get; set; }
+        public double SampleQty { get; set; }
+        public double SampleQtyOut { get; set; }
+        public string UnitDOType { get; set; }
+        public string ExpenditureType { get; set; }
+        public long URNItemId { get; set; }
+        public long UENItemId { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public string POSerialNumber { get; set; }
 
     }
 }
