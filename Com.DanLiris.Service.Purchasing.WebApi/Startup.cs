98165601c3cb90445f0367d4d8c5041ad2e634ce
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-11-03 11:14:43 +0700

    Enhance-Add-Report-MonitoringLogHistoryDMS

--- Com.DanLiris.Service.Purchasing.WebApi/Startup.cs
+++ Com.DanLiris.Service.Purchasing.WebApi/Startup.cs
@@ -239,7 +239,7 @@ namespace Com.DanLiris.Service.Purchasing.WebApi
                 .AddTransient<IGarmentLocalPurchasingJournalReportFacade, GarmentLocalPurchasingJournalReportFacade>()
                 .AddTransient<IGarmentDetailImportPurchasingJournalReportFacade, GarmentDetailImportPurchasingJournalReportFacade>()
                 .AddTransient<IGarmentDetailLocalPurchasingJournalReportFacade, GarmentDetailLocalPurchasingJournalReportFacade>()
-                .AddTransient<LogHistoryFacades>();
+                .AddTransient<ILogHistoryFacades, LogHistoryFacades>();
         }
 
         private void RegisterServices(IServiceCollection services, bool isTest)
