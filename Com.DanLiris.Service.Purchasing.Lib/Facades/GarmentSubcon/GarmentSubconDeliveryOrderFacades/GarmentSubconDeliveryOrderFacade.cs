7d0d30a6eca12a7d0216aa24391b11846643bda5
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-02-16 10:49:26 +0700

    Enhance-Add-MonitoringDOSubcon

--- Com.DanLiris.Service.Purchasing.Lib/Facades/GarmentSubcon/GarmentSubconDeliveryOrderFacades/GarmentSubconDeliveryOrderFacade.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Facades/GarmentSubcon/GarmentSubconDeliveryOrderFacades/GarmentSubconDeliveryOrderFacade.cs
@@ -4,6 +4,7 @@ using Com.DanLiris.Service.Purchasing.Lib.Helpers.ReadResponse;
 using Com.DanLiris.Service.Purchasing.Lib.Interfaces.GarmentSubcon;
 using Com.DanLiris.Service.Purchasing.Lib.Models.GarmentPurchaseRequestModel;
 using Com.DanLiris.Service.Purchasing.Lib.Models.GarmentSubconDeliveryOrderModel;
+using Com.DanLiris.Service.Purchasing.Lib.ViewModels.GarmentDeliveryOrderNonPOViewModel;
 using Com.DanLiris.Service.Purchasing.Lib.ViewModels.GarmentSubcon.GarmentSubconDeliveryOrderViewModel;
 using Com.Moonlay.Models;
 using Com.Moonlay.NetCore.Lib;
@@ -11,6 +12,9 @@ using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -54,8 +58,8 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentSubconDeliveryOrder
                     LastModifiedUtc = x.LastModifiedUtc,
                     RONo = x.RONo,
                     BeacukaiNo = x.BeacukaiNo,
-                    BeacukaiType =x.BeacukaiType,
-                    BeacukaiDate =x.BeacukaiDate ,
+                    BeacukaiType = x.BeacukaiType,
+                    BeacukaiDate = x.BeacukaiDate,
                     Items = x.Items.Select(y => new GarmentSubconDeliveryOrderItem
                     {
                         Id = y.Id,
@@ -107,7 +111,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentSubconDeliveryOrder
                     foreach (var item in m.Items)
                     {
                         EntityExtension.FlagForCreate(item, user, USER_AGENT);
-                        if(item.PRItemId != 0)
+                        if (item.PRItemId != 0)
                         {
                             var pr = dbSetPRItem.FirstOrDefault(x => x.Id == item.PRItemId);
                             pr.RemainingQuantity -= item.DOQuantity;
@@ -143,11 +147,11 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentSubconDeliveryOrder
 
 
                     //Delete Item or Update Item Old Data
-                    foreach(var item in oldModel.Items)
+                    foreach (var item in oldModel.Items)
                     {
                         var existItem = newModel.Items.FirstOrDefault(x => x.Id == item.Id);
 
-                        if(existItem == null)
+                        if (existItem == null)
                         {
                             EntityExtension.FlagForDelete(item, user, USER_AGENT);
 
@@ -161,7 +165,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentSubconDeliveryOrder
                         }
                         else
                         {
-                            if(item.DOQuantity != existItem.DOQuantity)
+                            if (item.DOQuantity != existItem.DOQuantity)
                             {
                                 if (item.PRItemId != 0)
                                 {
@@ -178,14 +182,14 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentSubconDeliveryOrder
 
                                 item.DOQuantity = existItem.DOQuantity;
                                 EntityExtension.FlagForUpdate(item, user, USER_AGENT);
-                            }          
+                            }
                         }
                     }
 
                     //Add new Item
-                    foreach(var item in newModel.Items)
+                    foreach (var item in newModel.Items)
                     {
-                        if(item.Id == 0)
+                        if (item.Id == 0)
                         {
                             if (item.PRItemId != 0)
                             {
@@ -204,7 +208,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentSubconDeliveryOrder
                     }
 
                     //Update Header Statement
-                    if(oldModel.DONo != newModel.DONo)
+                    if (oldModel.DONo != newModel.DONo)
                     {
                         oldModel.DONo = newModel.DONo;
                     }
@@ -310,7 +314,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentSubconDeliveryOrder
                 {
                     Id = m.Id,
                     DONo = m.DONo,
-                    RONo =m.RONo,
+                    RONo = m.RONo,
                     ProductOwnerId = m.ProductOwnerId,
                     ProductOwnerName = m.ProductOwnerName,
                     ProductOwnerCode = m.ProductOwnerCode,
@@ -322,7 +326,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentSubconDeliveryOrder
                     Items = m.Items.Select(i => new GarmentSubconDeliveryOrderItem
                     {
                         Id = i.Id,
-                        POSerialNumber =i.POSerialNumber,
+                        POSerialNumber = i.POSerialNumber,
                         DOQuantity = i.DOQuantity,
                         PricePerDealUnit = i.PricePerDealUnit,
                         ProductId = i.ProductId,
@@ -330,7 +334,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentSubconDeliveryOrder
                         ProductName = i.ProductName,
                         ProductRemark = i.ProductRemark,
                         UomId = i.UomId,
-                        UomUnit =i.UomUnit,
+                        UomUnit = i.UomUnit,
                     }).ToList()
                 });
 
@@ -372,5 +376,159 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentSubconDeliveryOrder
 
             return new ReadResponse<object>(listData, Total, OrderDictionary);
         }
+
+        public IQueryable<GarmentDeliveryOrderReportViewModel> GetReportQueryDO(string no, DateTime? dateFrom, DateTime? dateTo, int offset)
+        {
+            DateTime DateFrom = dateFrom == null ? new DateTime(1970, 1, 1) : (DateTime)dateFrom;
+            DateTime DateTo = dateTo == null ? DateTime.Now : (DateTime)dateTo;
+
+            var Query = (from a in dbContext.GarmentSubconDeliveryOrders
+                         join i in dbContext.GarmentSubconDeliveryOrderItems on a.Id equals i.GarmentDOId
+
+                         join o in dbContext.GarmentBeacukaiItems on a.Id equals o.GarmentDOId into beaitems
+                         from oo in beaitems.DefaultIfEmpty()
+                         join r in dbContext.GarmentBeacukais on oo.BeacukaiId equals r.Id into beas
+                         from rr in beas.DefaultIfEmpty()
+
+                         join n in dbContext.GarmentSubconUnitReceiptNoteItems on i.Id equals n.DOItemId into p
+                         from URNItem in p.DefaultIfEmpty()
+                         join k in dbContext.GarmentSubconUnitReceiptNotes on URNItem.URNId equals k.Id into l
+                         from URN in l.DefaultIfEmpty()
+
+                         join u in dbContext.GarmentSubconUnitDeliveryOrderItems on URNItem.Id equals u.URNItemId into uu
+                         from UDOItem in uu.DefaultIfEmpty()
+                         join ui in dbContext.GarmentSubconUnitDeliveryOrders on UDOItem.UnitDOId equals ui.Id into udi
+                         from UDO in udi.DefaultIfEmpty()
+
+                         join uei in dbContext.GarmentSubconUnitExpenditureNoteItems on UDOItem.Id equals uei.UnitDOItemId into uenitem
+                         from UENItem in uenitem.DefaultIfEmpty()
+                         join ue in dbContext.GarmentSubconUnitExpenditureNotes on UENItem.UENId equals ue.Id into uen
+                         from UEN in uen.DefaultIfEmpty()
+                         where
+                          a.IsDeleted == false && i.IsDeleted == false && URN.IsDeleted == false && URNItem.IsDeleted == false && rr.IsDeleted == false
+                              && oo.IsDeleted == false && UDOItem.IsDeleted == false && UDO.IsDeleted == false && UENItem.IsDeleted == false && UEN.IsDeleted == false
+                              && a.DONo == (string.IsNullOrWhiteSpace(no) ? a.DONo : no)
+                              && a.DODate.AddHours(offset).Date >= DateFrom.Date
+                              && a.DODate.AddHours(offset).Date <= DateTo.Date
+                              && (rr.CustomsType == "BC 40" || rr.CustomsType == "BC 27")
+
+                         select new GarmentDeliveryOrderReportViewModel
+                         {
+                             no = a.DONo,
+                             doDate = a.DODate == null ? new DateTime(1970, 1, 1) : a.DODate,
+                             arrivalDate = a.ArrivalDate,
+                             roNo = a.RONo,
+                             poNo = i.POSerialNumber,
+                             dOQuantity = i.DOQuantity,
+                             doUom = i.UomUnit,
+                             productCode = i.ProductCode,
+                             productName = i.ProductName,
+
+                             URNNo = URN == null ? "-" : URN.URNNo,
+                             URNDate = URN == null ? new DateTime(1970, 1, 1) : URN.ReceiptDate,
+                             urnQuantity = URNItem == null ? 0 : URNItem.SmallQuantity,
+                             urnUom = URNItem == null ? "-" : URNItem.SmallUomUnit,
+
+                             UDONo = UDO == null ? "-" : UDO.UnitDONo,
+                             UDODate = UDO == null ? new DateTime(1970, 1, 1) : UDO.UnitDODate,
+                             udoQuantity = UDOItem == null ? 0 : UDOItem.Quantity,
+                             udoUom = UDOItem == null ? "-" : UDOItem.UomUnit,
+
+                             UENNo = UEN == null ? "-" : UEN.UENNo,
+                             UENDate = UEN == null ? new DateTime(1970, 1, 1) : UEN.ExpenditureDate,
+                             uenQuantity = UENItem == null ? 0 : UENItem.Quantity,
+                             uenUom = UENItem == null ? "-" : UENItem.UomUnit,
+
+                             BeacukaiNo = rr != null ? rr.BeacukaiNo : "-",
+                             BeacukaiDate = rr != null ? rr.BeacukaiDate : DateTimeOffset.MinValue,
+                             BeacukaiType = rr != null ? rr.CustomsType : "-",
+
+                         });
+
+            //var QueryGroup = Query.GroupBy(x => x.)
+
+            return Query; ;
+        }
+
+        public Tuple<List<GarmentDeliveryOrderReportViewModel>, int> GetReportDO(string no, DateTime? dateFrom, DateTime? dateTo, int page, int size,string Order, int offset)
+
+        {
+            var Query = GetReportQueryDO(no, dateFrom, dateTo, offset);
+
+
+            Dictionary<string, string> OrderDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(Order);
+            if (OrderDictionary.Count.Equals(0))
+            {
+                Query = Query.OrderByDescending(b => b.doDate).ThenByDescending(b => b.UDODate);
+            }
+
+
+            Pageable<GarmentDeliveryOrderReportViewModel> pageable = new Pageable<GarmentDeliveryOrderReportViewModel>(Query, page - 1, size);
+            List<GarmentDeliveryOrderReportViewModel> Data = pageable.Data.ToList<GarmentDeliveryOrderReportViewModel>();
+            int TotalData = Query.Count();
+
+            return Tuple.Create(Query.ToList(), TotalData);
+        }
+
+        public MemoryStream GenerateExcelDO(string no, DateTime? dateFrom, DateTime? dateTo, int offset)
+        {
+            var Query = GetReportQueryDO(no, dateFrom, dateTo, offset);
+            Query = Query.OrderByDescending(b => b.doDate).ThenByDescending(b => b.UDODate);
+            DataTable result = new DataTable();
+            result.Columns.Add(new DataColumn() { ColumnName = "No", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Nomor Surat Jalan", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Tanggal Surat Jalan", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Tanggal Tiba", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Nomor RO", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Nomor PO", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Kode Barang", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Nama Barang", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Jumlah Dipesan", DataType = typeof(Double) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Satuan", DataType = typeof(String) });
+
+            result.Columns.Add(new DataColumn() { ColumnName = "No BC", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Tipe BC", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Tanggal Input BC", DataType = typeof(String) });
+
+            result.Columns.Add(new DataColumn() { ColumnName = "Nomor BUM", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Tanggal BUM", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Qty BUM", DataType = typeof(Double) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Satuan2", DataType = typeof(String) });
+
+            result.Columns.Add(new DataColumn() { ColumnName = "Nomor Unit DO", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Tanggal Unit DO", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Qty Unit DO", DataType = typeof(Double) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Satuan3", DataType = typeof(String) });
+
+            result.Columns.Add(new DataColumn() { ColumnName = "Nomor BUK", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Tanggal BUK", DataType = typeof(String) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Qty BUK", DataType = typeof(Double) });
+            result.Columns.Add(new DataColumn() { ColumnName = "Satuan4", DataType = typeof(String) });
+
+
+
+            if (Query.ToArray().Count() == 0)
+                result.Rows.Add("", "", "", "", "", "", "", "", 0, "", "", "", "", 0, "", "", "", "", "", "", 0, "", "", "", "", 0, "", "", "", "", 0, "");
+            else
+            {
+                int index = 0;
+                foreach (var item in Query)
+                {
+                    index++;
+                    string date = item.arrivalDate == null ? "-" : item.arrivalDate.ToOffset(new TimeSpan(offset, 0, 0)).ToString("dd MMM yyyy", new CultureInfo("id-ID"));
+                    string supplierDoDate = item.doDate == new DateTime(1970, 1, 1) ? "-" : item.doDate.ToOffset(new TimeSpan(offset, 0, 0)).ToString("dd MMM yyyy", new CultureInfo("id-ID"));
+                    string URNDate = item.URNDate == new DateTime(1970, 1, 1) ? "-" : item.URNDate.ToOffset(new TimeSpan(offset, 0, 0)).ToString("dd MMM yyyy", new CultureInfo("id-ID"));
+                    string UDODate = item.UDODate == new DateTime(1970, 1, 1) ? "-" : item.UDODate.ToOffset(new TimeSpan(offset, 0, 0)).ToString("dd MMM yyyy", new CultureInfo("id-ID"));
+                    string BcDate = item.BeacukaiDate == DateTimeOffset.MinValue ? "-" : item.BeacukaiDate.ToOffset(new TimeSpan(offset, 0, 0)).ToString("dd MMM yyyy", new CultureInfo("id-ID"));
+                    string UENDate = item.UENDate == new DateTime(1970, 1, 1) ? "-" : item.UENDate.ToOffset(new TimeSpan(offset, 0, 0)).ToString("dd MMM yyyy", new CultureInfo("id-ID"));
+
+                    result.Rows.Add(index, item.no, supplierDoDate, date, item.roNo, item.poNo, item.productCode, item.productName, item.dOQuantity, item.doUom, item.BeacukaiNo, item.BeacukaiType, BcDate, item.URNNo, URNDate, item.urnQuantity, item.urnUom, item.UDONo, UDODate, item.udoQuantity, item.udoUom, item.UENNo, UENDate, item.uenQuantity, item.uenUom);
+                }
+            }
+
+            return Excel.CreateExcel(new List<KeyValuePair<DataTable, string>>() { new KeyValuePair<DataTable, string>(result, "Territory") }, true);
+        }
     }
+
+
 }
