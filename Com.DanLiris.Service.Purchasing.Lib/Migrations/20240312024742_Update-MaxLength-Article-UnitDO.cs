d841eb2b84ff6933528a5a2a8ae545e887a8bb15
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-03-12 09:50:26 +0700

    Sprint6-Fix-MaxLength-Article-UnitDO

---
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20240312024742_Update-MaxLength-Article-UnitDO.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
+{
+    public partial class UpdateMaxLengthArticleUnitDO : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Article",
+                table: "GarmentUnitDeliveryOrders",
+                maxLength: 5000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Article",
+                table: "GarmentUnitDeliveryOrders",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 5000,
+                oldNullable: true);
+        }
+    }
+}
