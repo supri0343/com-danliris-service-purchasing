d4072422f07d348dce8fb8d7e2f1f56ddd3fca0c
ardana96 <ardanagama@gmail.com>
2024-08-12 15:46:52 +0700

    Enhancement Garment Disposition Add Invoice - SPRINT 16 - 1

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20240812071223_add_column_invoice_GarmentDispositionPurchaseItem.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20240812071223_add_column_invoice_GarmentDispositionPurchaseItem.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240812071223_add_column_invoice_GarmentDispositionPurchaseItem")]
+    partial class add_column_invoice_GarmentDispositionPurchaseItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
