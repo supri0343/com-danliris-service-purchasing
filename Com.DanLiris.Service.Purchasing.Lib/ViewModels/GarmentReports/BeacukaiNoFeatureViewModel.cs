f3c325e386b4bbae10e0b3f5349ffdbbf6680f22
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-09-19 13:32:29 +0700

    2024/20/Enhance/Feature-BCNo-FIxQuery
    -Add Parameter
    
    2024/20/Enhance/Feature-BCNo
    
    2024/20/Enhance/Feature-BCNo
    
    -Left Join urn and unitDO
    
    2024/20/Enhance/Feature-BCNo
    
    - Penambahan kolom untuk di select pada menu pencarian no bc

--- Com.DanLiris.Service.Purchasing.Lib/ViewModels/GarmentReports/BeacukaiNoFeatureViewModel.cs
+++ Com.DanLiris.Service.Purchasing.Lib/ViewModels/GarmentReports/BeacukaiNoFeatureViewModel.cs
@@ -17,5 +17,16 @@ namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.GarmentReports
         public string Construction { get; set; }
         public double QtyBC { get; set; }
 
+        //Enhance 19-09-2024 
+        public string SupplierName { get; set; }
+        public double Bruto { get; set; }
+        public double Netto { get; set; }
+        public string URNNo { get; set; }
+        public DateTime? URNDate { get; set; }
+        public string UENNo { get; set; }
+        public DateTime? UENDate { get; set; }
+        public double QtyUEN { get; set; }
+        public string UENType { get; set; }
+        public long UnitDOItemId { get; set; }
     }
 }
