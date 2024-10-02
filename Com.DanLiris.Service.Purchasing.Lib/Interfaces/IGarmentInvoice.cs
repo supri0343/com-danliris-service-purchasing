bbf14e9d8672ddf1f76f0903e6fa58138f2d34dd
ardana96 <ardanagama@gmail.com>
2024-10-02 09:57:13 +0700

    Enhancement Invoice merge InternNote - SPRINT 21 - 01

--- Com.DanLiris.Service.Purchasing.Lib/Interfaces/IGarmentInvoice.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Interfaces/IGarmentInvoice.cs
@@ -11,10 +11,16 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Interfaces
     {
         Tuple<List<GarmentInvoice>, int, Dictionary<string, string>> Read(int Page = 1, int Size = 25, string Order = "{}", string Keyword = null, string Filter = "{}");
         GarmentInvoice ReadById(int id);
-        Task<int> Create(GarmentInvoice m, GarmentInvoiceViewModel viewModel, string user, int clientTimeZoneOffset = 7 );
+        Task<int> Create(GarmentInvoice model, string username, int clientTimeZoneOffset = 7);
+        Task<int> CreateMerge(GarmentInvoice m, GarmentInvoiceViewModel viewModel, string user, int clientTimeZoneOffset = 7 );
         Task<int> Update(int id, GarmentInvoice m, string user, int clientTimeZoneOffset = 7);
         int Delete(int id, string username);
         GarmentInvoice ReadByDOId(int id);
         List<GarmentInvoice> ReadForInternNote(List<long> garmentInvoiceIds);
+        Tuple<List<GarmentInvoiceIndexDto>, int, Dictionary<string, string>> ReadMerge(int Page = 1, int Size = 25, string Order = "{}", string Keyword = null, string Filter = "{}");
+        string GetInternNoteNo(long invoiceId);
+
+
+
     }
 }
