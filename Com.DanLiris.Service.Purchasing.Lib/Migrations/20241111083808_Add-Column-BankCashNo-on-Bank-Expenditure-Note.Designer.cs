9ad5cfbf5a08abbe7263c266a9acb626bf4d91b4
AndreaZain <andrea.kinan@gmail.com>
2024-11-14 09:25:53 +0700

    Enhance-Transaction-And-Report-Kas-Bank-Payment-Dispo
    Enhance Bukti Pemgeluaran kas Bank DPP & PPN & Report Textile

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20241111083808_Add-Column-BankCashNo-on-Bank-Expenditure-Note.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20241111083808_Add-Column-BankCashNo-on-Bank-Expenditure-Note.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241111083808_Add-Column-BankCashNo-on-Bank-Expenditure-Note")]
+    partial class AddColumnBankCashNoonBankExpenditureNote
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
