8a70782a1f52a8bcb27287819a575e988731c7a7
AndreaZain <andrea.kinan@gmail.com>
2024-11-20 13:43:16 +0700

    Enhance-Monitoring-Price-And-Delivery-Pengiriman
    Create Create Monitoring Price & Delivery Pengiriman

---
+++ Com.DanLiris.Service.Purchasing.Lib/ViewModels/GarmentDeliveryOrderViewModel/GarmentMonitoringDeliveryReportViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.GarmentDeliveryOrderViewModel
+{
+   public class GarmentMonitoringDeliveryReportViewModel
+    {
+        public string epoNo { get; set; }
+        public DateTimeOffset orderDate { get; set; }
+        public DateTimeOffset deliveryDate { get; set; }        
+        public string createdBy { get; set; }
+        public string supplierCode { get; set; }
+        public string supplierName { get; set; }
+        public string prNo { get; set; }
+        public string poNo { get; set; }
+        public string productCode { get; set; }
+        public string productName { get; set; }
+        public string productRemark { get; set; }
+        public string currencyCode { get; set; }
+        public string paymentType { get; set; }
+        public string category { get; set; }
+        public double dealQuantity { get; set; }
+        public string uomUnit { get; set; }
+        public double doEpoQuantity { get; set; }
+        public double epoPrice { get; set; }
+        public string doNo { get; set; }
+        public string doDate { get; set; }
+        public string arrivalDate { get; set; }
+        public double doQuantity { get; set; }
+        public int statusDO { get; set; }
+        public int diffDay { get; set; }
+        public int sevenddaybefore { get; set; }
+        public string flagData { get; set; }
+    }
+}
